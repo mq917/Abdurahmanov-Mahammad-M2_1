298f011c1b225094c82268efc7d050d32eb026c3
mq917 <61832580+mq917@users.noreply.github.com>
2020-03-26 20:30:35 +0400

    Update index.js

--- index.js
+++ index.js
@@ -24,8 +24,41 @@ function result() {
         document.getElementById('str').innerHTML = ("срок введен неправильно");
         console.log('wrong type');
         return NaN;
+    }document.getElementById('butt').onclick = result;
+    function result() {
+    let startAmount = Number(document.getElementById('s').value);
+    let amountOfMonth = Number(document.getElementById('m').value);
+    let depositYield = Number(document.getElementById('p').value);
+    let depositTermDay = Number(document.getElementById('t').value);
+    if (startAmount < 0 || Number.isNaN(startAmount)) {
+        document.getElementById('str').innerHTML = ("начальная сумма указано неправильно");
+        console.log('wrong type');
+        return NaN;
+    } 
+    if (amountOfMonth < 0 || Number.isNaN(amountOfMonth)) {
+        document.getElementById('str').innerHTML = ("сумма пополнения введено неправильно");
+        console.log('wrong type');
+        return NaN;
     }
-
+    if (depositYield <= 0 || Number.isNaN(depositYield)) {
+        document.getElementById('str').innerHTML = ("неправильный формат процентовой ставки");
+        console.log('wrong type');
+        return NaN;
+    }
+    if (depositTermDay < 0 || Number.isNaN(depositTermDay)) {
+        document.getElementById('str').innerHTML = ("срок введен неправильно");
+        console.log('wrong type');
+        return NaN;
+    } 
+	calculate(startAmount, amountOfMonth, depositYield , depositTermDay);
+	}
+    function calculate (startAmount, amountOfMonth, depositYield , depositTermDay  ) {
+    let a = Math.floor(depositTermDay / 30);
+    for (let k = 0; k < a; k++) {
+        startAmount = startAmount + amountOfMonth + startAmount * depositYield / 100 / 12;
+    }
+    document.getElementById('str').innerHTML = startAmount;
+    }
     let a = Math.floor(depositTermDay / 30);
     for (let k = 0; k < a; k++) {
         startAmount = startAmount + amountOfMonth + startAmount * depositYield / 100 / 12;
